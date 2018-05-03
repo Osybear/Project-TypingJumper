6202af4f1235dfc79508a318618fa092503ae7f2
Osybear <osybeargames@gmail.com>
2018-05-03 00:40:18 -0500

    Update
    Added "help" option
    
    Updated options text
    
    Changed "move left" option to "left"
    and "move right" option to "right"

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,20 +10,29 @@ public class PlayerController : MonoBehaviour
     public Rigidbody2D m_PlayerRB;
     public InputField m_InputField;
 
+    public GameObject m_HelpOptions;
+
     private float m_JumpForce = 6.5f;
     private float m_Speed = 5f;
 
     public bool m_MoveLeft = false;
     public bool m_MoveRight = false;
 
+    private void Awake() {
+        m_HelpOptions.SetActive(false);
+    }
+
     private void Update()
     {
         GUIUtility.systemCopyBuffer = null;
+        
+        if(m_InputField.isFocused == false)
+            m_InputField.ActivateInputField();
+
         if (m_InputField.text != null && Input.GetKeyDown(KeyCode.Return))
         {
             ProccessInput(m_InputField.text);
             m_InputField.text = null;
-            m_InputField.ActivateInputField();
         }
 
         MoveLeft();
@@ -41,13 +50,13 @@ public class PlayerController : MonoBehaviour
         if (text == "jump right")
             JumpRight();
 
-        if (text == "move left")
+        if (text == "left")
         {
             m_MoveLeft = true;
             m_MoveRight = false;
         }
 
-        if (text == "move right")
+        if (text == "right")
         {
             m_MoveRight = true;
             m_MoveLeft = false;
@@ -65,6 +74,8 @@ public class PlayerController : MonoBehaviour
 
         if (text == "exit")
             SceneManager.LoadScene("menu");
+        if(text == "help")
+            m_HelpOptions.SetActive(true);
     }
 
     private void MoveLeft()
@@ -92,5 +103,9 @@ public class PlayerController : MonoBehaviour
     private void JumpRight()
     {
         m_PlayerRB.AddForce(new Vector2(m_JumpForce/2f, m_JumpForce), ForceMode2D.Impulse);
+    }
+
+    public void CloseHelp(){
+        m_HelpOptions.SetActive(false);
     }
 }
